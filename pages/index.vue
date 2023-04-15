05b3acafc20594ee62c6ce473dd3dab6e11068d8
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2023-04-15 22:40:57 +0300

    ss

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,5 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 <script setup>
-await navigateTo("/authentication/signin/basic.vue");
+await navigateTo("/authentication/signin/basic");
 </script>
